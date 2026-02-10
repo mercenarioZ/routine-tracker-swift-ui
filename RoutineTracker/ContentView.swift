f2f4851bc6715e3f2c00a196c6238ca39b1de403
Nai <nai@litmers.com>
2026-02-10 13:19:48 +0700

    Initial Commit

---
+++ RoutineTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RoutineTracker
+//
+//  Created by Le Ba Nguyen Vu on 10/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RoutineTracker/RoutineTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineTrackerApp.swift
+//  RoutineTracker
+//
+//  Created by Le Ba Nguyen Vu on 10/2/26.
+//
+
+import SwiftUI
+
+@main
+struct RoutineTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
